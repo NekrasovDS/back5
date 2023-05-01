e87d277ccbd53699ca49803f36ff7709f56f67bd
NekrasovDS <112845788+NekrasovDS@users.noreply.github.com>
2023-05-02 00:46:19 +0300

    Update login.php

--- login.php
+++ login.php
@@ -1,58 +1,42 @@
 <?php
-echo "<link rel='stylesheet' href='style.css'>";
 header('Content-Type: text/html; charset=UTF-8');
 session_start();
 if (!empty($_SESSION['login'])) {
-    header('Location: index.php');
+  session_destroy();
+  header('Location: ./');
 }
+$message;
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 ?>
-<form action="login.php" method="post">
-  <input name="login" />
-  <input name="password" type ="password"/>
-  <input type="submit" value="Войти" />
-</form>
-
-
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Задание 5 - логин скрин</title>
+  <link rel="stylesheet" href="style.css"><script src="script.js"></script>
+  </head>
+  <div>
+<form action="" method="post">
+  <label>Логин<input name="login" /></label>
+  <label>Пароль<input name="pass" /></label>
+  <input type="submit" value="Войти" /></form></div>
 <?php
-}
+if (!empty($_GET['none'])) {
+  $txt="Таких данных в бд НЕТ";
+    print"<div>" . $txt . "<div>";}}
 else {
-    $login=$_POST['login'];
-    $pswrd=$_POST['password'];
-    $uid=0;
-    $error=TRUE;
-    $user = 'u52806';
-    $pass = '7974759';
-    $db1 = new PDO('mysql:host=localhost;dbname=u52806', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
-    if(!empty($login) and !empty($pswrd)){
-        
-      
-      try{
-            $check=$db1->prepare("SELECT * FROM login WHERE login=?");
-            $check->bindParam(1,$login);
-            $check->execute();
-            $username=$check->fetchALL();
-            if(password_verify($pswrd,$username[0][2])){
-                $uid=$username[0]['id'];
-                $error=FALSE;
-            }
-        }
-        catch(PDOException $e){
-            print('Error : ' . $e->getMessage());
-            exit();
-        }
-    }
-  
-  
-  
-    if($error==TRUE){
-        print('Неправильные логин или пароль <br> Создайте новый <a href="index.php"> аккаунт </a> или <a href="login.php"> повторите поытку входа </a> ');
-        session_destroy();
-        exit();
-    }
-
-
-  $_SESSION['login'] = $login;
-  $_SESSION['uid'] = $uid;
-  header('Location: index.php');
-}
+  $user = 'u52806';
+  $pass = '7974759';
+  $db = new PDO('mysql:host=localhost;dbname=u52806', $user, $pass, array(PDO::ATTR_PERSISTENT => true));
+  $login_id = $_POST['login'];
+  $pass = md5($_POST['pass']);
+  $stmt = $db->prepare("SELECT user_id FROM login_data WHERE login_id = ? AND pass = ?");
+  $stmt->execute([$login_id, $pass]);
+  $user_id = $stmt ->fetch(PDO::FETCH_COLUMN);
+  if($user_id){
+    $_SESSION['login'] = $_POST['login'];
+    $_SESSION['uid'] = $user_id;
+    $_COOKIE[session_name()] = "session_true";
+    header('Location: ./');}
+  else{
+    header('Location: ?none=1');}}
+?>
